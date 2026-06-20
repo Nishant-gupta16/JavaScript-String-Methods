7143eb47e0d9344a60a9c7af448faafa257a1eaa
Nishant-gupta16 <168644246+Nishant-gupta16@users.noreply.github.com>
2026-06-20 22:08:26 +0530

    Comment out all string method examples in stringMethods.js

--- stringMethods.js
+++ stringMethods.js
@@ -1,1 +1,77 @@
+// const newStr= new String(`Nishant`);
+// console.log(newStr.__proto__);
+
+
+// const newStr= new String(`Nishant`);
+// let b="Gupta"
+// console.log(newStr.concat(" ",b));
+// console.log(`My name is ${newStr} ${b}`)
+
+
+// const newStr= new String(`Nishant`);
+// console.log(newStr.includes('Nishan'));
+
+// const newStr= new String(`Nishant`);
+// console.log(newStr.startsWith(`Nish`));
+
+
+// const newStr= new String(`Nishant`);
+// console.log(newStr.endsWith(`nt`));
+
+// const newStr= new String(`Nishanta`);
+// console.log(newStr.indexOf(`a`));
+
+// const newStr= new String(`Nishanta`);
+// console.log(newStr.lastIndexOf(`a`));
+
+// const newStr= new String(`Nishant`);
+// console.log(newStr.slice(0,4));
+
+// const newStr= new String(`Nishant`);
+// console.log(newStr.substring(3,7));
+
+
+// const newStr= new String(`Nishant`);
+// console.log(newStr.substr(1,5));
+
+// const newStr= new String(`Hello Gupta`);
+// console.log(newStr.replace(`Hello`,`Nishant`));
+
+// const newStr= new String(`Gupta`);
+// console.log(newStr.replaceAll(`Gupta`,`Nishant Gupta`));
+
+// const newStr= new String(`Nishant,hii,gupta,jii`);
+// console.log(newStr.split(`,`));
+
+// const newStr= new String(`Nishant`);
+// console.log(newStr.toUpperCase());
+
+
+// const newStr= new String(`Nishant`);
+// console.log(newStr.toLowerCase());
+
+
+// const newStr= new String(`   Nishant         `)
+// console.log(newStr.trim());
+
+
+// const newStr= new String(`Nishant`);
+// console.log(newStr.padStart(10,`x`));
+
+// const newStr= new String(`Nishant`);
+// console.log(newStr.padEnd(9,`x`));
+
+// let str=`Nis `;
+// console.log(str.repeat(3))
+
+// let str=`Nishant Gupta`
+// console.log(str.match(`Gupta`))
+
+// let str= `Apple`
+// console.log(str.localeCompare(`banana`))
+
+
+// let str= `Hello mr 420`;
+// console.log(str.search(/\d/));
+
 
